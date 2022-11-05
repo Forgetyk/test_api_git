3a638e2bf855f57a9f0e419341e91b828ac9b96e
forgetyk <itt2g@mail.ru>
2022-11-05 12:57:29 +0500

    add pr

--- main.py
+++ main.py
@@ -6,8 +6,8 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name} выфвфы')  # Press Ctrl+F8 to toggle the breakpoint.
 
-def dsad dsadsa
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
