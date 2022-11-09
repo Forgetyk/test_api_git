451b2e37dc90fc51e4fec7de5816b5016b8bb1c6
Forgetyk <89727234+Forgetyk@users.noreply.github.com>
2022-11-09 14:43:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name} выфвфы HOTFIX')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name} выфвфы HOTFIXaaa')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def new_func(task):
     print("new")
