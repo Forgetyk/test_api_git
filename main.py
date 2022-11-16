94c6756405f75855a646ab86f5c48db34eb9da93
forgetyk <itt2g@mail.ru>
2022-11-16 22:46:35 +0500

    dsa

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import re
+import subprocess
 import sys
 import git
 import yaml
 
 
 def init():
+    global playbooks
     vars = []
     roles = []
     playbooks = {}
@@ -35,7 +37,8 @@ def init():
             for role in roles:
                 if role in yaml.safe_load(playbook)[0]["roles"]:
                     tmp.append(role)
-            playbooks[str(file)] = tmp
+            if len(tmp) != 0:
+                playbooks[str(file)] = tmp
     for key in playbooks.keys():
         if re.search(r"git", key) is None:
             with open(f"{main_dir}\\{key}", 'r') as stream:
@@ -48,12 +51,34 @@ def init():
                         print(f"  {host}")
 
 
+def lint():
+    for playbook in playbooks.keys():
+        subprocess.Popen(["ansible-lint", playbook, "-p", "--nocolor", "|", "ansible-lint-junit", "-o", "ansible-lint.xml"])
+
+
+def check():
+    for playbook in playbooks.keys():
+        subprocess.Popen(["ansible-playbook", playbook, "--check"])
+
+
+def run():
+    for playbook in playbooks.keys():
+        subprocess.Popen(["ansible-playbook", playbook])
+
+
 if __name__ == '__main__':
     global list_changed
     global main_dir
     main_dir = "\\".join(os.path.abspath(__file__).split("\\")[:-1])
-    list_changed = git.Repo('').git.diff('HEAD~0', '--name-only').split("\n")
+    list_changed = git.Repo('').git.diff('HEAD~1', '--name-only').split("\n")
     if len(list_changed) == 0:
         print("no modified files found")
         sys.exit(1)
-    init()
+    if sys.argv[1] == "--init":
+        init()
+    if sys.argv[1] == "--lint":
+        lint()
+    if sys.argv[1] == "--check":
+        check()
+    if sys.argv[1] == "--run":
+        run()
