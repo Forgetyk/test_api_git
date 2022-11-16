380f207c891eb6f1ebb8bcc6f9fd9e370c21efc7
forgetyk <itt2g@mail.ru>
2022-11-16 13:12:37 +0500

    dsa

--- main.py
+++ main.py
@@ -12,23 +12,23 @@ def init():
     for file in list_changed:
         if file[-4:] != "yaml" and file[-3:] != "yml":
             continue
-        with open(f"{main_dir}/{file}", mode="r", encoding="utf-8") as file:
+        with open(f"{main_dir}\\{file}", mode="r", encoding="utf-8") as file:
             for line in file:
                 if re.fullmatch(r"^\S+:\n", line):
                     vars.append(line[:-2])
-    for root, dir, files in os.walk(f"{main_dir}/roles"):
+    for root, dir, files in os.walk(f"{main_dir}\\roles"):
         for file in files:
             if file[-4:] != "yaml" and file[-3:] != "yml":
                 continue
-            with open(f"{root}/{file}", mode="r", encoding="utf-8") as file_role:
+            with open(f"{root}\\{file}", mode="r", encoding="utf-8") as file_role:
                 for var in vars:
                     if var in file_role.read():
-                        roles.append(root.split("roles/")[1].split("/")[0])
+                        roles.append(root.split("roles\\")[1].split("\\")[0])
     roles = list(set(roles))
     for file in os.listdir(main_dir):
         if file[-4:] != "yaml" and file[-3:] != "yml":
             continue
-        with open(f"{main_dir}/{file}", mode="r", encoding="utf-8") as playbook:
+        with open(f"{main_dir}\\{file}", mode="r", encoding="utf-8") as playbook:
             tmp = []
             for role in roles:
                 if role in yaml.safe_load(playbook)[0]["roles"]:
@@ -36,7 +36,7 @@ def init():
             playbooks[str(file)] = tmp
     for key in playbooks.keys():
         if re.search(r"git", key) is None:
-            with open(f"{main_dir}/{key}", 'r') as stream:
+            with open(f"{main_dir}\\{key}", 'r') as stream:
                 hosts = yaml.safe_load(stream)[0]["hosts"]
                 if type(hosts) == str:
                     print(f"{key}:\n  {hosts}")
@@ -49,7 +49,7 @@ def init():
 if __name__ == '__main__':
     global list_changed
     global main_dir
-    main_dir = "/".join(os.path.abspath(__file__).split("/")[:-1])
+    main_dir = "\\".join(os.path.abspath(__file__).split("\\")[:-1])
     list_changed = git.Repo('').git.diff('HEAD~1', '--name-only').split("\n")
     if len(list_changed) == 0:
         print("no modified files found")
